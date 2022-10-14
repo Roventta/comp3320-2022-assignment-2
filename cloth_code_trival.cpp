10d75566f903d1908a1712f164fbf4f3d5739c8e
Heming Zhu <hz6100@gadi-login-03.gadi.nci.org.au>
2022-10-14 13:31:32 +1100

    modified force calculation

--- cloth_code_trival.cpp
+++ cloth_code_trival.cpp
@@ -1,5 +1,6 @@
 #include "./cloth_code.h"
 #include "./SoAVectorLib.cpp"
+#include "./experiments/bitMagicExperiment.cpp"
 
 #include <math.h>
 #include <stdio.h>
@@ -88,16 +89,10 @@ void loopcode(int n, double mass, double fcon, int delta, double grav,
     for (i = 0; i < n; i++) {
       x[j * n + i] += dt * (vx[j * n + i] + dt * fx[j * n + i] * 0.5 / mass);
       oldfx[j * n + i] = fx[j * n + i];
-    }
-  }
-  for (j = 0; j < n; j++) {
-    for (i = 0; i < n; i++) {
+      
       y[j * n + i] += dt * (vy[j * n + i] + dt * fy[j * n + i] * 0.5 / mass);
       oldfy[j * n + i] = fy[j * n + i];
-    }
-  }
-  for (j = 0; j < n; j++) {
-    for (i = 0; i < n; i++) {
+     
       z[j * n + i] += dt * (vz[j * n + i] + dt * fz[j * n + i] * 0.5 / mass);
       oldfz[j * n + i] = fz[j * n + i];
     }
@@ -106,23 +101,28 @@ void loopcode(int n, double mass, double fcon, int delta, double grav,
   //	apply constraints - push cloth outside of ball
   for (j = 0; j < n; j++) {
     for (i = 0; i < n; i++) {
-      xdiff = x[j * n + i] - xball;
-      ydiff = y[j * n + i] - yball;
-      zdiff = z[j * n + i] - zball;
-      vmag = sqrt(xdiff * xdiff + ydiff * ydiff + zdiff * zdiff);
-      if (vmag < rball) {
-        x[j * n + i] = xball + xdiff * rball / vmag;
-        y[j * n + i] = yball + ydiff * rball / vmag;
-        z[j * n + i] = zball + zdiff * rball / vmag;
-        
+	xdiff = x[j * n + i] - xball;
+	ydiff = y[j * n + i] - yball;
+	zdiff = z[j * n + i] - zball;
+	vmag = sqrt(xdiff * xdiff + ydiff * ydiff + zdiff * zdiff);
+	
+	long mask = (EBH_dtl(vmag-rball)>>63);
+	x[j*n+i] = EBH_ltd((EBH_dtl(x[j*n+i]) & ~mask) | (EBH_dtl(xball+xdiff*rball/vmag) & mask));
+	y[j*n+i] = EBH_ltd((EBH_dtl(y[j*n+i]) & ~mask) | (EBH_dtl(yball+ydiff*rball/vmag) & mask));
+	z[j*n+i] = EBH_ltd((EBH_dtl(z[j*n+i]) & ~mask) | (EBH_dtl(zball+zdiff*rball/vmag) & mask));
+
 	//get the normal of the ball contacting surface
 	struct singleVector surfaceNormal = vectorSub(xball, yball, zball, x[j*n+i], y[j*n+i], z[j*n+i]);
 	struct singleVector eliminated_velocity = vectorProj(vx[j*n+i], vy[j*n+i], vz[j*n+i], surfaceNormal.x, surfaceNormal.y, surfaceNormal.z);
 	struct singleVector newVelocity = vectorSub(vx[j*n+i], vy[j*n+i], vz[j*n+i],eliminated_velocity.x, eliminated_velocity.y, eliminated_velocity.z);
 	//friction
 	newVelocity = vectorScale(newVelocity.x, newVelocity.y, newVelocity.z, 0.1);
-	loadSingle(vx, vy, vz, j*n+i , newVelocity);
-      }
+	
+	vx[j*n+i] = EBH_ltd((EBH_dtl(vx[j*n+i]) & ~mask) | (EBH_dtl(newVelocity.x) & mask));
+	vy[j*n+i] = EBH_ltd((EBH_dtl(vy[j*n+i]) & ~mask) | (EBH_dtl(newVelocity.y) & mask));
+	vz[j*n+i] = EBH_ltd((EBH_dtl(vz[j*n+i]) & ~mask) | (EBH_dtl(newVelocity.z) & mask));
+
+    
     }
   }
 
@@ -157,8 +157,11 @@ void loopcode(int n, double mass, double fcon, int delta, double grav,
 double eval_pef(int n, int delta, double mass, double grav, double sep,
                 double fcon, double *x, double *y, double *z, double *fx,
                 double *fy, double *fz) {
-  double pe, rlen, xdiff, ydiff, zdiff, vmag;
+  double pe, xdiff, ydiff, zdiff, vmag;
   int nx, ny, dx, dy;
+
+  int adjacents[1+4*delta*delta+4*delta] = {0};
+  double rlen[1+4*delta*delta+4*delta] = {0};
 
   pe = 0.0;
   // loop over particles
@@ -168,14 +171,13 @@ double eval_pef(int n, int delta, double mass, double grav, double sep,
       fy[nx * n + ny] = 0.0;
       fz[nx * n + ny] = -mass * grav;
       // loop over displacements
+      /*
       for (dx = MAX(nx - delta, 0); dx < MIN(nx + delta + 1, n); dx++) {
         for (dy = MAX(ny - delta, 0); dy < MIN(ny + delta + 1, n); dy++) {
           // exclude self interaction
           if (nx != dx || ny != dy) {
             // compute reference distance
-            rlen =
-                sqrt((double)((nx - dx) * (nx - dx) + (ny - dy) * (ny - dy))) *
-                sep;
+            rlen = sqrt((double)((nx - dx) * (nx - dx) + (ny - dy) * (ny - dy))) * sep;
             // compute actual distance
             xdiff = x[dx * n + dy] - x[nx * n + ny];
             ydiff = y[dx * n + dy] - y[nx * n + ny];
@@ -188,7 +190,30 @@ double eval_pef(int n, int delta, double mass, double grav, double sep,
             fz[nx * n + ny] += fcon * zdiff * (vmag - rlen) / vmag;
           }
         }
-      }
+      } */
+	int adjacentIndex = 0;
+	for (dx = MAX(nx - delta, 0); dx < MIN(nx + delta + 1, n); dx++) {
+        	for (dy = MAX(ny - delta, 0); dy < MIN(ny + delta + 1, n); dy++) {
+		       	if (nx != dx || ny != dy) {
+				adjacents[adjacentIndex] = dx*n+dy;
+            			rlen[adjacentIndex] = sqrt((double)((nx - dx) * (nx - dx) + (ny - dy) * (ny - dy))) * sep;
+				adjacentIndex++;
+			}
+		}
+	}
+	for (int i = 0; i<adjacentIndex; i++) {
+	    // compute reference distance
+            // compute actual distance
+            xdiff = x[adjacents[i]] - x[nx * n + ny];
+            ydiff = y[adjacents[i]] - y[nx * n + ny];
+            zdiff = z[adjacents[i]] - z[nx * n + ny];
+            vmag = sqrt(xdiff * xdiff + ydiff * ydiff + zdiff * zdiff);
+            // potential energy and force
+            pe += fcon * (vmag - rlen[i]) * (vmag - rlen[i]);
+            fx[nx * n + ny] += fcon * xdiff * (vmag - rlen[i]) / vmag;
+            fy[nx * n + ny] += fcon * ydiff * (vmag - rlen[i]) / vmag;
+            fz[nx * n + ny] += fcon * zdiff * (vmag - rlen[i]) / vmag;
+          }
     }
   }
   return 0.5 * pe;
